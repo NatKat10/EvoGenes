ac774357fdc30cd56bfae314b8da0c67feb27a03
ayemoy <ayelet431@gmail.com>
2024-05-18 18:16:34 +0300

    sss

--- frontend/src/components/HomeComp.vue
+++ frontend/src/components/HomeComp.vue
@@ -1,17 +1,5 @@
 <template>
-  <!-- <div class="container"> 
-    <div class="card rounded-pill text-center overflow-hidden" style="width: 18rem;">
-      <img src="../assets/evo1.png" class="card-img-top" alt="...">
-      <div class="card-body bg-dark text-white">
-        <h5 class="card-title">Run Yass</h5>
-        <p class="card-text">Something about the yass.</p>
-        <p class="mt-3">$129.00</p>
-        <a href="#" class="btn btn-light px-3 rounded-pill">Run Now</a>
-      </div>
-    </div>
-  </div> -->
 
-  <!-- <div class="container">  -->
     <div class="card rounded-pill text-center overflow-hidden" style="width: 18rem;">
       <img :src="imageUrl" class="card-img-top" alt="...">
       <div class="card-body bg-dark text-white">
