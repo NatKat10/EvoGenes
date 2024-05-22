af80411642b3c8055f3280faad9ad19841bc7bc4
assaf7070 <76042716+assaf7070@users.noreply.github.com>
2024-05-22 14:05:41 +0300

    Gene view and structure updated with zoom capabilities

---
+++ backend/GeneImage.py
@@ -0,0 +1,223 @@
+import json
+import matplotlib.pyplot as plt
+import mpld3
+import numpy as np 
+
+
+
+"""
+MIT License
+Copyright (c) [2016] [Parashar Dhapola]
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+__author__ = "Parashar Dhapola"
+__email__ = "parashar.dhapola@gmail.com"
+
+
+class GeneImage(object):
+    def __init__(self, exon_intervals, marker_pos=[], marker_heights=[], marker_colors=[],
+                 marker_size=100, marker_weight=1.5, exon_color="black", intron_color="grey",
+                 intron_weight=2, intron_style='-', bar_color='cornflowerblue', bg_color="white"):
+        self.exonIntervals = exon_intervals
+        self.markerPositions = marker_pos
+        self.markerHeights = marker_heights
+        self.markerColors = marker_colors
+        self.markerSize = marker_size
+        self.MarkerWeight = marker_weight
+        self.exonColor = exon_color
+        self.intronColor = intron_color
+        self.intronWeight = intron_weight
+        self.intronStyle = intron_style
+        self.barColor = bar_color
+        self.bgColor = bg_color
+        self.markerDefaultColor = 'grey'
+        self.numExons = len(self.exonIntervals)
+        self.totalSpan = self.exonIntervals[-1][1] - self.exonIntervals[0][0]
+        self.minExonLen = self.totalSpan * 0.005
+        self.ylims = {'exon_max': 1, 'exon_min': -1.5}
+        self.figure, self.canvas = plt.subplots(figsize=(15, 0.5))
+        self.canvas.set_facecolor(self.bgColor)
+        self._draw()
+
+    def _set_limits(self):
+        self.ylims['intron_max'] = self.ylims['exon_max'] * 0.9
+        self.ylims['intron_min'] = (self.ylims['exon_max'] + self.ylims['exon_min']) / 2.0
+        self.ylims['bar_min'] = self.ylims['exon_max'] + 0.2
+        self.ylims['bar_max'] = self.ylims['bar_min'] + (self.ylims['exon_max'] - self.ylims['exon_min']) / 5.0
+
+    def _transform_spans(self):
+        span_lens = [x[1] - x[0] for x in self.exonIntervals]
+        max_len = float(max(span_lens))
+        transformed_intervals = []
+        if max_len < self.minExonLen:
+            span_ratios = [x / max_len for x in span_lens]
+            expansion_factor = self.totalSpan * 1e-11
+            for i in range(1, 10):
+                ef = (2 ** i) * expansion_factor
+                if max_len + ef > self.minExonLen:
+                    expansion_factor = ef
+                    break
+            for i, j in zip(self.exonIntervals, span_ratios):
+                mid = (i[0] + i[1]) / 2
+                f = (expansion_factor * j) / 2
+                if mid + f - mid - f > self.minExonLen:
+                    transformed_intervals.append([mid - f, mid + f])
+                else:
+                    transformed_intervals.append([mid - (self.minExonLen / 2), mid + (self.minExonLen / 2)])
+        else:
+            for i in range(self.numExons):
+                if span_lens[i] < self.minExonLen:
+                    mid = (self.exonIntervals[i][0] + self.exonIntervals[i][0]) / 2
+                    transformed_intervals.append([mid - (self.minExonLen / 2), mid + (self.minExonLen / 2)])
+                else:
+                    transformed_intervals.append(self.exonIntervals[i])
+        self.exonIntervals = transformed_intervals[:]
+
+    def _draw_exon(self, span):
+        self.canvas.fill_between(span, self.ylims['exon_min'], self.ylims['exon_max'],
+                                 edgecolor=self.bgColor, facecolor=self.exonColor)
+        return True
+
+    def _draw_intron(self, span):
+        mid = (span[0] + span[1]) / 2.0
+        self.canvas.plot([span[0], mid], [self.ylims['intron_min'], self.ylims['intron_max']],
+                         c=self.intronColor, lw=self.intronWeight, ls=self.intronStyle)
+        self.canvas.plot([mid, span[1]], [self.ylims['intron_max'], self.ylims['intron_min']],
+                         c=self.intronColor, lw=self.intronWeight, ls=self.intronStyle)
+        return True
+
+    def _draw_markers(self):
+        if self.markerHeights == []:
+            self.markerHeights = [self.ylims['exon_max'] - self.ylims['exon_min'] for x in self.markerPositions]
+        if self.markerColors == []:
+            self.markerColors = [self.markerDefaultColor for x in self.markerPositions]
+        for p, h, c in zip(self.markerPositions, self.markerHeights, self.markerColors):
+            # self.canvas.plot((p, p), (self.ylims['bar_max'], self.ylims['bar_max'] + h),
+            #                  linestyle='-', color='black', linewidth=self.MarkerWeight, alpha=0.7)
+            # self.canvas.scatter(p, self.ylims['bar_max'] + h + 0.25, s=self.markerSize, marker='o', c=c,
+            #                     edgecolor=c, alpha=1)
+            self.canvas.scatter(p, self.ylims['bar_max'] + h + 1,  c=c,
+                                 alpha=1)
+
+    # def _clean_axes(self):
+    #     # self.canvas.set_ylim((self.ylims['exon_min'], self.ylims['bar_max']))
+    #     self.canvas.set_yticks([], [])
+    #     self.canvas.get_xaxis().tick_top()
+    #     self.canvas.tick_params(axis='x', direction='out')
+    #     self.canvas.set_xticks([])
+    #     for o in ["top", "bottom", "left", "right"]:
+    #         self.canvas.spines[o].set_visible(False)
+    #     min_pos = int(self.exonIntervals[0][0] - self.totalSpan * 0.1)
+    #     if min_pos < 0:
+    #         min_pos = 0
+    #     max_pos = int(self.exonIntervals[-1][1] + self.totalSpan * 0.1)
+    #     minortick_pos = [x for x in range(min_pos, max_pos, int((max_pos - min_pos) / 20))][1:]
+    #     for i in minortick_pos:
+    #         self.canvas.axvline(i, alpha=0.2, c='black', ls='--')
+    #     self.canvas.text(minortick_pos[0], self.ylims['exon_min'] - 0.5,
+    #                      minortick_pos[0], fontsize=8, ha='center')
+    #     self.canvas.text(minortick_pos[-1], self.ylims['exon_min'] - 0.5,
+    #                      minortick_pos[-1], fontsize=8, ha='center')
+    #     self.canvas.set_xlim(minortick_pos[0] - (minortick_pos[1] - minortick_pos[0]),
+    #                          minortick_pos[-1] + (minortick_pos[-1] - minortick_pos[-2]))
+    
+    
+    def _clean_axes(self):
+        self.canvas.set_yticks([], [])
+        self.canvas.get_xaxis().tick_top()
+        self.canvas.tick_params(axis='x', direction='out')
+        self.canvas.set_xticks([])
+        for o in ["top", "bottom", "left", "right"]:
+            self.canvas.spines[o].set_visible(False)
+
+        # Calculate the minimum and maximum x-axis limits
+        min_pos = self.exonIntervals[0][0]
+        max_pos = self.exonIntervals[-1][1]
+
+        # Adjust the minimum and maximum x-axis limits
+        x_range = max_pos - min_pos
+        adjusted_min_pos = min_pos - 0.05 * x_range
+        adjusted_max_pos = max_pos + 0.05 * x_range
+
+        # Set the adjusted x-axis limits
+        self.canvas.set_xlim(adjusted_min_pos, adjusted_max_pos)
+
+        # Calculate minor tick positions
+        num_minor_ticks = 20  # You can adjust this value as needed
+        minor_tick_step = x_range / (num_minor_ticks - 1)
+        minor_tick_positions = np.arange(min_pos, max_pos + minor_tick_step, minor_tick_step)
+
+        # Draw minor ticks
+        for pos in minor_tick_positions:
+            self.canvas.axvline(pos, alpha=0.2, c='black', ls='--')
+
+        # Add labels for the first and last minor tick positions
+        self.canvas.text(min_pos, self.ylims['exon_min'] - 0.5, int(min_pos), fontsize=8, ha='left')
+        self.canvas.text(max_pos, self.ylims['exon_min'] - 0.5, int(max_pos), fontsize=8, ha='right')
+
+    def _draw(self):
+        self._set_limits()
+        self._transform_spans()
+        for i in range(self.numExons):
+            if i > 0:
+                self._draw_intron([self.exonIntervals[i - 1][1], self.exonIntervals[i][0]])
+            self._draw_exon(self.exonIntervals[i])
+        # self.canvas.fill_between([self.exonIntervals[0][0], self.exonIntervals[-1][1]],
+        #                          self.ylims['bar_min'], self.ylims['bar_max'],
+        #                          edgecolor=self.bgColor, facecolor=self.barColor)
+        # self._draw_fs()
+        self._clean_axes()
+
+    def show(self):
+        plt.show()
+
+    def save_plot(self, output_path='gene_image.png'):
+        """
+        Save the gene image plot to a file.
+        :param output_path: Path to save the plot image.
+        """
+        plt.savefig(output_path)
+
+    def show_mpld3(self):
+        mpld3.show()
+
+    def get_figure_html(self):
+        # Create the Matplotlib figure
+        self.figure, self.canvas = plt.subplots(figsize=(15, 0.5))
+        self.canvas.set_facecolor(self.bgColor)
+
+        # Call the internal functions to draw the plot
+        self._set_limits()
+        self._transform_spans()
+        self._draw()
+
+        # Convert the figure to HTML using mpld3
+        figure_html = mpld3.fig_to_html(self.figure)
+
+        return figure_html
+
+
+
+# geneImage = GeneImage() 
+# if  __name__ == "__main__":
+#     #exons positions
+#     exon_pos = [97543299,97544702], [97547885,97548026], [97564044,97564188], [97658624,97658804], [97700407,97700550], [97770814,97770934], [97771732,97771853], [97839116,97839200], [97847948,97848017], [97915614,97915779], [97981281,97981497], [98015115,98015300], [98039315,98039526], [98058773,98058943], [98060614,98060722], [98144650,98144738], [98157272,98157354], [98164906,98165103], [98187065,98187227], [98205947,98206035], [98293669,98293752], [98348819,98348930], [98386439,98386615]
+#     #marker positions
+#     marker_pos = [97947885, 98247485]
+#     gene = GeneImage(exon_pos, marker_pos)
+#     gene.show()
---
+++ backend/routes.py
@@ -0,0 +1,275 @@
+from flask import Blueprint, request, jsonify, render_template, Flask
+# from . import db
+from .models import Gene
+from .schemas import gene_schema, genes_schema
+# import requests, sys, json
+import requests, sys, json
+import logging
+
+from flask_cors import CORS
+from flask_cors import cross_origin
+
+import os
+import mpld3
+
+
+# #--------------------
+# from .GeneImage import GeneImage
+# import matplotlib
+# matplotlib.use('Agg')  # Use the 'Agg' backend for non-interactive mode
+# import matplotlib.pyplot as plt
+# import io
+# from flask import send_file
+# #--------------------
+
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True, allow_headers=["Content-Type", "Authorization"], methods=["GET", "POST", "OPTIONS"])
+
+#Blueprints
+main = Blueprint('main', __name__)
+generate = Blueprint('generate', __name__)
+@generate.route('/test', methods=['GET'])
+def test():
+    return "Blueprint is working!"
+
+@main.route('/', methods=['GET'])
+def get_data():
+    return jsonify({"Hello": "World"})
+
+import os
+from flask import send_file,send_from_directory
+
+from flask import jsonify, request,send_file,current_app
+from .extensions import db  # Make sure this import is correct based on your setup
+from .models import Gene  # Adjust this import if necessary
+
+from .GeneImage import GeneImage  # Import the GeneImage class
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), '..', '..', 'backend')
+
+import subprocess
+import os
+import tempfile
+from flask import Response
+import io  
+import re
+
+
+
+
+
+
+
+@main.route('/run-yass', methods=['POST'])
+def run_yass():
+    #to show the real output of yass you need to comment this lines.
+    # image_directory = '../'  # This navigates up from 'backend/app' to 'backend'
+    # image_filename = 'example.png'
+    # try:
+    #     return send_from_directory(image_directory, image_filename)
+    # except Exception as e:
+    #     return jsonify({"error": str(e)}), 500
+
+    fasta_file1_path = 'temp_sequence1.fasta'
+    fasta_file2_path = 'temp_sequence2.fasta'
+
+    if 'fasta1' in request.files and 'fasta2' in request.files:
+        fasta_file1 = request.files['fasta1']
+        fasta_file2 = request.files['fasta2']
+        fasta_file1.save(fasta_file1_path)
+        fasta_file2.save(fasta_file2_path)
+    elif 'sequence1' in request.form and 'sequence2' in request.form:
+        sequence1 = request.form['sequence1']
+        sequence2 = request.form['sequence2']
+        # Validate sequence input
+        if not re.match('^[ACGTacgt]*$', sequence1) or not re.match('^[ACGTacgt]*$', sequence2):
+            return jsonify({'error': 'Invalid sequence input. Sequences should only contain A, C, G, T characters.'}), 400
+        with open(fasta_file1_path, 'w') as file1, open(fasta_file2_path, 'w') as file2:
+            file1.write(f'>Sequence1\n{sequence1}\n')
+            file2.write(f'>Sequence2\n{sequence2}\n')
+    else:
+        return jsonify({'error': 'No valid sequence or file input provided'}), 400
+
+
+
+    # Proceed with YASS processing if inputs are valid
+    yass_output_path = 'yass_output.yop'
+    dp_output_path = 'dp.png'
+    yass_executable = './yass-Win64.exe'
+
+    command = [yass_executable, fasta_file1_path, fasta_file2_path, '-o', yass_output_path]
+    subprocess.run(command, check=True)
+    
+    # php_script = 'yass2dotplot.php'
+    # subprocess.run(['php', php_script, yass_output_path, dp_output_path], check=True)
+    
+
+    # python_executable = 'python'
+    python_executable = os.path.join(os.environ['VIRTUAL_ENV'], 'Scripts', 'python.exe')  # For Windows
+    yop_reader_script = './yop_reader.py'
+
+    # yop_reader_script = os.getcwd()+'\\backend\\yop_reader.py'
+
+    # yop_reader_script = os.path.join(os.getcwd(), 'backend', 'yop_reader.py')
+
+
+    command = [python_executable, yop_reader_script, yass_output_path, dp_output_path]
+    subprocess.run(command, check=True)
+
+    with open(dp_output_path, 'rb') as file:
+        image_data = file.read()
+    
+    os.remove(fasta_file1_path)
+    os.remove(fasta_file2_path)
+    os.remove(yass_output_path)
+    os.remove(dp_output_path)
+    
+    return Response(image_data, mimetype='image/png')
+
+
+    # code for file input only
+    # fasta_file1 = request.files['fasta1']
+    # fasta_file2 = request.files['fasta2']
+    
+    # # Save files to temporary paths
+    # fasta_file1_path = 'temp_sequence1.fasta'
+    # fasta_file2_path = 'temp_sequence2.fasta'
+    # fasta_file1.save(fasta_file1_path)
+    # fasta_file2.save(fasta_file2_path)
+    
+    # # Define output paths
+    # yass_output_path = 'yass_output.yop'
+    # dp_output_path = 'dp.png'
+    
+    # # Run YASS with the FASTA files
+    # yass_executable = './yass-Win64.exe'
+    # command = [yass_executable, fasta_file1_path, fasta_file2_path, '-o', yass_output_path]
+    # subprocess.run(command, check=True)
+    
+    # # Convert YOP to DotPlot
+    # php_script = 'yass2dotplot.php'  # Make sure this script is executable and has a .php extension
+    # subprocess.run(['php', php_script, yass_output_path, 'filename1="gene1"', 'filename2="gene2"', dp_output_path], check=True)
+    
+    # # Send the DotPlot image file as a response
+    # with open(dp_output_path, 'rb') as file:
+    #     image_data = file.read()
+    
+    # # Delete temporary files
+    # os.remove(fasta_file1_path)
+    # os.remove(fasta_file2_path)
+    # os.remove(yass_output_path)
+    # os.remove(dp_output_path)
+    
+    # return Response(image_data, mimetype='image/png')
+
+
+@main.route('/add', methods=['POST'])
+def add_gene():
+    data = request.get_json()
+
+    # Check if the request JSON contains 'gene_name' and 'sequence'
+    if not data or 'gene_name' not in data or 'sequence' not in data:
+        return jsonify({"error": "Missing gene_name or sequence"}), 400
+
+    # Create a new Gene object with the data
+    new_gene = Gene(gene_name=data['gene_name'], sequence=data['sequence'])
+
+    # Add the new Gene object to the session and commit it to the database
+    db.session.add(new_gene)
+    db.session.commit()
+
+    # Return a response indicating success, including the added gene's ID
+    return jsonify({"message": "Gene added successfully", "gene_id": new_gene.id}), 201
+
+
+
+
+@main.route('/genes', methods=['GET'])
+def get_genes():
+    # Query all gene records from the database
+    all_genes = Gene.query.all()
+    # Serialize the list of gene records to JSON
+    result = genes_schema.dump(all_genes)
+    # Return the serialized gene records as JSON
+    return jsonify(result)
+
+@generate.route('/generate', methods=['POST'])
+def run_generate():
+    data = request.get_json()
+    sequence1 = data.get('sequence1', '')
+    sequence2 = data.get('sequence2', '')
+
+    # Implementation of the YASS algorithm logic here
+    # ...
+
+    # For now, return a placeholder response
+    return jsonify({"message": "Graph generated successfully"})
+
+@generate.route('/get-gene-seq', methods=['POST'])
+def get_gene_seq():
+    data = request.get_json()
+    gene_id = data.get('gene_id', '')
+
+    if not gene_id:
+        return jsonify({"error": "Missing gene_id"}), 400
+
+    content_type = 'application/json'
+    x_fasta_content = fetch_endpoint(gene_id, content_type)
+
+    return jsonify({"x_fasta_content": x_fasta_content})
+
+def fetch_endpoint(gene_ensembl_id, content_type):
+    server = "http://rest.ensembl.org/"
+    request = f"/sequence/id/{gene_ensembl_id}?"
+    r = requests.get(server + request, headers={"Accept": content_type})
+
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
+
+    return r.json()
+
+@generate.route('/gene-structure', methods=['POST'])
+@cross_origin()
+def get_gene_structure():
+    data = request.get_json()
+    gene_id = data.get('gene_id', '')
+
+    if not gene_id:
+        logging.error("Missing gene_id in get_gene_structure route")
+        return jsonify({"error": "Missing gene_id"}), 400
+
+    logging.info(f"Fetching gene structure for gene_id: {gene_id}")
+    gene_structure = fetch_gene_structure(gene_id)
+    return jsonify(gene_structure)
+
+
+def fetch_gene_structure(gene_ensembl_id, content_type='application/json'):
+    server = "http://rest.ensembl.org"
+    exon_endpoint = f"/overlap/id/{gene_ensembl_id}?feature=exon"
+    r = requests.get(server + exon_endpoint, headers={"Accept": content_type})
+
+    try:
+        r.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+        gene_structure = r.json()  # Parse JSON response
+        return gene_structure
+    except requests.exceptions.HTTPError as e:
+        logging.error(f"Error fetching gene structure for gene_id {gene_ensembl_id}: {e}")
+        return None  # Return None to indicate failure
+    
+
+@generate.route('/gene-image', methods=['POST'])
+@cross_origin()
+def plot_gene_image():
+    data = request.json
+    exons_positions = data.get('exonsPositions', [])
+    
+    # Generate the gene image plot
+    gene = GeneImage(exons_positions[0], exons_positions[0][0])
+    
+    # Get the HTML code for the interactive figure
+    figure_html = gene.get_figure_html()
+    
+    # Return the HTML code as a response
+    return Response(figure_html, content_type='text/html')
