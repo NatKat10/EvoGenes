44c824f28100f3e113d7030421db150abc32bf02
ayemoy <ayelet431@gmail.com>
2024-06-27 12:40:21 +0300

    Instructions page

--- frontend/src/components/AboutComp.vue
+++ frontend/src/components/AboutComp.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="container">
-    <h1>Comprehensive End-to-End Gene Visualization System - "Evo-Genes"</h1>
+    <h1>"Evo-Genes" Web Instructions</h1>
     <p>
-      As part of our final project in the Department of Software and Information Systems Engineering at Ben Gurion University of the Negev, under the guidance of Prof. Guy Shani from the Faculty of Software and Information Systems Engineering and Dr. Tal Shay from the Faculty of Life Sciences, we have developed a comprehensive end-to-end gene visualization system. The laboratory aims to unravel the complexity of how cells with identical genetic information can exhibit diverse functions and phenotypic properties. 
+      LOLOLO
     </p>
-    <p>
-      A significant challenge in this research is the limited capability to compare gene similarities across species while maintaining the context of the original gene structure.
-    </p>
-    <p>
-      Currently, no system allows for the simultaneous examination of sequence similarities and structural differences between species. To address this challenge, we have created a web-based system that leverages both server-side and client-side programming to interface with the extensive Ensembl database.
-    </p>
-    <p>
-      Our system facilitates intuitive and visually engaging comparative gene sequence analysis, emphasizing the display of exons, thus optimizing comparative gene analysis. This unique feature significantly enhances the accessibility and utility of gene comparison tools, providing biologists and researchers with comprehensive insights into gene development and functional divergence. The system streamlines the comparative analysis process by automating sequence retrieval and aligning gene structures with Dot Plot outputs, thereby boosting research productivity and data analysis efficiency.
-    </p>
-    <p>
-      We have invested considerable effort in the design and development of the system, ensuring a professional and attractive user experience, which is integral to its overall functionality.
-    </p>
+    
   </div>
 </template>
 
--- frontend/src/pages/GeneSeqPage.vue
+++ frontend/src/pages/GeneSeqPage.vue
@@ -1,85 +1,16 @@
 <template>
   <div class="seq">
     <div class="cont">
-      <div class="grid">
-        <PopularGenesComp
-          :imageUrl="require('../assets/logoo.jpg')"
-          :title="'Popular Human Genes 1'"
-          :description="'See the most popular genes for human use'"
-          :buttonLabel="'HUMAN'"
-          :modalTitle="'Details for Popular Human Genes 1'"
-          :tableData="[
-            { id: 1, col1: 'Gene A1', col2: 'Sequence A1' },
-            { id: 2, col1: 'Gene A2', col2: 'Sequence A2' }
-          ]"
-        />
-        <PopularGenesComp
-          :imageUrl="require('../assets/logoo.jpg')"
-          :title="'Popular Human Genes 2'"
-          :description="'See the most popular genes for human use'"
-          :buttonLabel="'HUMAN'"
-          :modalTitle="'Details for Popular Human Genes 2'"
-          :tableData="[
-            { id: 1, col1: 'Gene B1', col2: 'Sequence B1' },
-            { id: 2, col1: 'Gene B2', col2: 'Sequence B2' }
-          ]"
-        />
-        <PopularGenesComp
-          :imageUrl="require('../assets/logoo.jpg')"
-          :title="'Popular Human Genes 3'"
-          :description="'See the most popular genes for human use'"
-          :buttonLabel="'HUMAN'"
-          :modalTitle="'Details for Popular Human Genes 3'"
-          :tableData="[
-            { id: 1, col1: 'Gene C1', col2: 'Sequence C1' },
-            { id: 2, col1: 'Gene C2', col2: 'Sequence C2' }
-          ]"
-        />
-        <PopularGenesComp
-          :imageUrl="require('../assets/logoo.jpg')"
-          :title="'Popular Human Genes 4'"
-          :description="'See the most popular genes for human use'"
-          :buttonLabel="'HUMAN'"
-          :modalTitle="'Details for Popular Human Genes 4'"
-          :tableData="[
-            { id: 1, col1: 'Gene D1', col2: 'Sequence D1' },
-            { id: 2, col1: 'Gene D2', col2: 'Sequence D2' }
-          ]"
-        />
-        <PopularGenesComp
-          :imageUrl="require('../assets/logoo.jpg')"
-          :title="'Popular Human Genes 5'"
-          :description="'See the most popular genes for human use'"
-          :buttonLabel="'HUMAN'"
-          :modalTitle="'Details for Popular Human Genes 5'"
-          :tableData="[
-            { id: 1, col1: 'Gene E1', col2: 'Sequence E1' },
-            { id: 2, col1: 'Gene E2', col2: 'Sequence E2' }
-          ]"
-        />
-        <PopularGenesComp
-          :imageUrl="require('../assets/logoo.jpg')"
-          :title="'Popular Human Genes 6'"
-          :description="'See the most popular genes for human use'"
-          :buttonLabel="'HUMAN'"
-          :modalTitle="'Details for Popular Human Genes 6'"
-          :tableData="[
-            { id: 1, col1: 'Gene F1', col2: 'Sequence F1' },
-            { id: 2, col1: 'Gene F2', col2: 'Sequence F2' }
-          ]"
-        />
-      </div>
+      
     </div>
   </div>
 </template>
 
 <script>
-import PopularGenesComp from "@/components/PopularGenesComp.vue";
 
 export default {
   name: 'gene-sequence',
   components: {
-    PopularGenesComp
   },
 }
 </script>
